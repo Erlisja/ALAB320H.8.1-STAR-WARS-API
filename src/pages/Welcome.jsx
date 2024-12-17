3d34ac0a945a4199683115d8e9d3a2536ea9c668
Erlisja Kore <korelisa@yahoo.com>
2024-12-16 19:35:06 -0500

    Added the welocme page

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router"
+
+function Welcome() {
+  return (
+    <div className="main-div">
+        <div className="title">
+        Welcome to Star Wars Library
+        </div>
+        <h2>
+            To get started, click on the Starships link above to browse through the list of Star Wars Starships.
+
+        </h2>
+        </div>
+  )
+}
+
+export default Welcome
